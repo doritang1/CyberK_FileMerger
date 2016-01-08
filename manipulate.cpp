98f0cee60f7b57b5e86f0da5dc4f8b88bf228f01
CyberK <doritang1@naver.com>
2016-01-08 18:32:19 +0900

    verify code updated.
    code for verifying is updated.

--- manipulate.cpp
+++ manipulate.cpp
@@ -1,7 +1,7 @@
 #include <manipulate.h>
 #include <QStringMatcher>
 #include <QStringList>
-
+#include <QtCore>
 QString manipulate(QString str)
 {
     str.remove("\n");
@@ -17,6 +17,7 @@ QString manipulate(QString str)
 
     for(;;)
     {
+        //qDebug()<<"오프셋:"+QString::number(offset)+"\n";
         matcher.setPattern(tokenBegin);
 
         /*offset은 검색 시작위치로 첫번째 타이틀 검색시에는 0이고,
@@ -33,16 +34,18 @@ QString manipulate(QString str)
 
         matcher.setPattern(tokenEnd);
         int indexTitleLength = matcher.indexIn(str,offset)-indexTitleBegin;
-
+        qDebug()<<"길이:"+QString::number(indexTitleLength)+"\n";
         strlstTitles.append(str.mid(indexTitleBegin,indexTitleLength));
         offset += indexTitleLength; //첫번째 타이틀의 마지막으로 검색시작위치를 옮김
     }
 
-    if(strlstTitles.at(1).isEmpty()) //만약 2번째 타이틀이 없다면 첫번째 타이틀에 그냥 탭을 넣어준다.
+    if(strlstTitles.length()<2) //만약 2번째 타이틀이 없다면 첫번째 타이틀에 그냥 탭을 넣어준다.
     {
-        str = strlstTitles.at(0)+ "\t" + str;
-    } else { //만약 2번째 타이틀이 있다면 두번째 타이틀에 움라우트표시로 *를 넣고 탭을 넣어준다.
-        str = strlstTitles.at(1)+("*\t") + str;
+        str = strlstTitles.at(0)+ "\t" + str + "\n";
+    } else if(strlstTitles.at(1).isEmpty()){ //만약 2번째 타이틀이 있다면 두번째 타이틀에 움라우트표시로 *를 넣고 탭을 넣어준다.
+        str = strlstTitles.at(0)+ "\t" + str + "\n";
+    } else {
+        str = strlstTitles.at(1)+("*\t") + str + "\n";
     }
 
     return str;
